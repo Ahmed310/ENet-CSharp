01570b327b5ff18db713c8fb935a164ca28b3ba4
Ahmed PC <m.ahmed310@hotmail.com>
2026-04-08 20:55:54 +0500

    Android build fix 16k page

--- Source/Native/jni/Application.mk
+++ Source/Native/jni/Application.mk
@@ -1,6 +1,14 @@
-APP_PLATFORM := android-31
+APP_PLATFORM := android-21
+
+APP_ABI := armeabi-v7a arm64-v8a x86_64
+APP_STL := none
+
 APP_OPTIM := release
-APP_MODULES := libenet
-APP_ABI := arm64-v8a
-APP_CFLAGS := -fPIC
-APP_LDFLAGS := -Wl,-z,max-page-size=16384
+APP_SHORT_COMMANDS := true
+
+APP_CPPFLAGS += -fPIC
+APP_CFLAGS += -fPIC
+
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+  APP_LDFLAGS += -Wl,-z,max-page-size=16384
+endif
