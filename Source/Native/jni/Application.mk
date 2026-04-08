d6500fa07bab3cb10f5ba8fb3b0ade6902032569
Ahmed PC <m.ahmed310@hotmail.com>
2026-04-08 17:57:02 +0500

    Application.mk updated to support 16k page alignment

--- Source/Native/jni/Application.mk
+++ Source/Native/jni/Application.mk
@@ -1,3 +1,6 @@
-APP_PLATFORM := android-18
+APP_PLATFORM := android-31
 APP_OPTIM := release
 APP_MODULES := libenet
+APP_ABI := arm64-v8a
+APP_CFLAGS := -fPIC
+APP_LDFLAGS := -Wl,-z,max-page-size=16384
